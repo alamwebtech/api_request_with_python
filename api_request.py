7d3034957101f1948b157c6073318a897f61922b
Md Alam <alamwebtech@gmail.com>
2019-02-03 17:04:48 -0500

    pulling out random jokes using api

--- api_request.py
+++ api_request.py
@@ -1,5 +1,13 @@
 import requests 
+import pyfiglet
+from random import choice
+
+
 user_input = input("What would you like to search for? ")
+
+
+header = pyfiglet.figlet_format(user_input)
+print(f"{header} jokes")
 
 url = "https://icanhazdadjoke.com/search"
 
@@ -10,11 +18,14 @@ res=requests.get(
 ).json()
 
 num_jokes = res["total_jokes"]
+results = res["results"]
 if num_jokes > 1:
-	print("There are many jokes")
+	print(f"I found {num_jokes} jokes about {user_input}. Here is one:")
+	print(choice(results)["joke"])
 elif num_jokes == 1:
-	print("There is one joke")
+	print(f"I found one joke about {user_input}")
+	print(results[0]["joke"])
 else: 
-	print("There are no jokes ")
+	print(f"There are no jokes with yoour term: {user_input}")
 
 # print(num_jokes)
